f23e7652b09f83370e8c0e35c8c22ad175b7b253
Pc Ivan <jgodksblessings@gmail.com>
2025-01-28 20:01:05 -0600

    save martes

---
+++ Semana 2/Martes.py
@@ -0,0 +1,7 @@
+
+Encabezados=["Fecha de nacimiento", "Nombre", "Lugar de nacimiento"]
+Alumno=["12 DE MAYO DE 2005", "Ivan","Veracurz"]
+AL=[Encabezados, Alumno]
+Alumnos={}
+Alumnos["2225467"]=[AL]
+print(Alumnos)
