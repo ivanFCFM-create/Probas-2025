0a75e50b3a67ed393652b47f5269d232d602dca1
Pc Ivan <jgodksblessings@gmail.com>
2025-01-30 13:24:26 -0600

    save act en salon in 'semana1'

---
+++ Semana1/Act en salon.py
@@ -0,0 +1,17 @@
+
+#Actividad 1 
+print("hola mundo")
+
+
+# Actividad 2
+
+a=int(input('Introduzca primer número:'))
+b=int(input('Introduzca el segundo número:'))
+
+
+
+print('Adición=',a+b)
+print('sustracción=', a-b)
+print('multiplicación=', a*b)
+print('División=',a/b)
+
