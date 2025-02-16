6d6c955ed72132b36a7880e2624d0daa3be0ec70
Pc Ivan <jgodksblessings@gmail.com>
2025-02-15 21:20:52 -0600

    save

--- 100 problemas/Problema 11.py
+++ 100 problemas/Problema 11.py
@@ -3,7 +3,8 @@
 
 Palabra=str(input('Introduzca una palabra:'))
 inversa= ''.join(reversed(Palabra))
-if Palabra==inversa:
+estructurada= inversa.replace()
+if Palabra==estructurada:
    print('La palabra es un palíndromo')
 else:
   print('No es un palíndormo')
